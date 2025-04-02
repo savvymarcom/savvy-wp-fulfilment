d30d9976c602accf79f0c83b9205574c6707e521
savvymarcom <robin@savvymarcom.com>
2025-04-02 11:10:58 +0100

    update version

--- savvy-web-fulfilment.php
+++ savvy-web-fulfilment.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     SavvyWeb Fulfilment
  * Description:     Custom fulfilment integration for Savvy Web.
- * Version:         1.3.2
+ * Version:         1.3.3
  * Author:          SavvyWeb Solutions
  */
 
